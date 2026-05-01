b7ece5d1cef61a5737b46cbaa73159efa21c87ee
lowellteddy <lowellteddy88@gmail.com>
2026-05-01 13:03:41 +0300

    changed firebase import

--- widows-flour/src/main.jsx
+++ widows-flour/src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
-import "./firebase";
+import "./src/firebase.js";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
